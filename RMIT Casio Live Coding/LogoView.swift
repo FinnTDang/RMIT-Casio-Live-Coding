0ab8a426522cee7c78d7ecf81f0c4d15884e1497
Finn Dang <finntd6501@gmail.com>
2024-08-31 20:34:18 +0700

    finish upto reels

---
+++ RMIT Casio Live Coding/LogoView.swift
@@ -0,0 +1,18 @@
+//
+//  LogoView.swift
+//  RMIT Casio Live Coding
+//
+//  Created by Cindy Truong on 31/8/2024.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogoView()
+}
---
+++ RMIT Casio Live Coding/Modifiers.swift
@@ -0,0 +1,8 @@
+//
+//  Modifiers.swift
+//  RMIT Casio Live Coding
+//
+//  Created by Cindy Truong on 31/8/2024.
+//
+
+import Foundation
