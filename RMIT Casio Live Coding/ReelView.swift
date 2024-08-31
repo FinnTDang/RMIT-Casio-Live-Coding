8768d3eb09c7781139893381549495541bdb6eb1
Finn Dang <finntd6501@gmail.com>
2024-09-01 05:45:21 +0700

    add logic and basic animation

---
+++ RMIT Casio Live Coding/ReelView.swift
@@ -0,0 +1,18 @@
+//
+//  ReelView.swift
+//  RMIT Casio Live Coding
+//
+//  Created by Cindy Truong on 1/9/2024.
+//
+
+import SwiftUI
+
+struct ReelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReelView()
+}
